a016f7116336155e235753680628a9e1ad903d9c
Nidhish Bhimrajka <54209827+nidh-ish@users.noreply.github.com>
2021-02-24 13:02:42 +0530

    Add files via upload

---
+++ AreaOfCircle.c
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+int main()
+{
+    float a,r;
+   scanf("%f", &r); 
+    if(r>=0)
+	{
+        printf("%.2f",a=3.14*r*r);
+	}
+    else
+    {
+	    printf("Invalid input");
+    }
+return 0;
+}
