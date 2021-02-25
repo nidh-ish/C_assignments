01aba1cd6e140d13ecf8c343cf57f4db83f81341
Nidhish Bhimrajka <54209827+nidh-ish@users.noreply.github.com>
2021-02-25 12:12:31 +0530

    Made the code more readable

--- RemainderOfTwo.c
+++ RemainderOfTwo.c
@@ -1,22 +1,25 @@
 #include<stdio.h>
-int main()
-{
+
+int main() {
+    
     int a,b,c,d;
+    
     scanf("%d %d", &a, &b);
-    if (a>0 && b>0)
-    {
-    if (a>b)
-    {
-    printf("%d",c=a%b);
+    
+    if (a > 0 && b > 0) {
+    
+        if (a > b) {
+            printf("%d", c = a % b);
+        }
+    
+        else {
+            printf("%d", d = b % a);
+        }
     }
-    else
-    {
-    printf("%d",d=b%a);
+    
+    else {
+        printf("Invalid input");
     }
-    }
-    else
-    {
-    printf("Invalid input");
-    }
+    
     return 0;
 }
