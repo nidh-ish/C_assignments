262580ede6ccc6d2b4631e896ecb130b24909e5e
Nidhish Bhimrajka <54209827+nidh-ish@users.noreply.github.com>
2021-02-26 00:24:18 +0530

    Made the code more readable

--- TemperatureConversion.c
+++ TemperatureConversion.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
-int main()
-{
-    double Tc,Tf;
+
+int main() {
+    
+    double Tc, Tf;
+    
     scanf("%lf", &Tc);
-    Tf=1.8*Tc+32;
-    printf("%.2lf",Tf);
+    
+    Tf = 1.8 * Tc + 32;
+    
+    printf("%.2lf", Tf);
+    
     return 0;
 }
