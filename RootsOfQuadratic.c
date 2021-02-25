4493a84bf3d5396a8c4fea3f44acfe2d9297d3f5
Nidhish Bhimrajka <54209827+nidh-ish@users.noreply.github.com>
2021-02-26 00:18:20 +0530

    Roots of quadratic equation in C language

---
+++ RootsOfQuadratic.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+    int main()
+{
+    double a,b,c,D;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    D=b*b-4*a*c;
+
+    if(a!=0)
+    {
+    double d,e;
+    d=((-b+sqrt(D))/(2*a));
+    e=((-b-sqrt(D))/(2*a));
+    	if (D<0){
+          double pr1=-b/(2*a);
+    	double pr2=(sqrt(-D)/(2*a));
+     	printf("%.2lf+i%.2lf %.2lf-i%.2lf",pr1,pr2,pr1,pr2);}
+    else{
+      printf("%.2lf %.2lf",d,e);}
+    }
+    return 0;
+}
