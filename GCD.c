39eb3d4423cd34ffe30ee94ab606b0438c59d63b
Nidhish Bhimrajka <54209827+nidh-ish@users.noreply.github.com>
2021-02-27 17:27:23 +0530

    Update GCD.c

--- GCD.c
+++ GCD.c
@@ -1,53 +1,54 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main()
-{
-	int a,b,q;
-	scanf("%d %d", &a, &b);
- a=abs(a);
-      b=abs(b);
+
+int main() {
 	
-	if(a!=0 && b!=0)
-     {	
-     
-   	if(a>b)
-	{	
-	while((a%b)!=0)
-	{
-	q=a%b;
-	a=b;
-	b=q;
-	}
-	printf("%d", b);
-	}
-	else if(b>a)
-	{	
-        while((b%a)!=0)
-        {
-        q=b%a;
-        b=a;
-        a=q;
-        }
-        printf("%d", a);
-        }
-        }
- 
-	else if(a==0 && b!=0)
-         {
-		printf("%d", b);
+	int a, b, q;
+	
+	scanf("%d %d", &a, &b);
+	
+	a = abs(a);
+	b = abs(b);
+	
+	if(a != 0 && b != 0) {	
 		
+		if(a > b) {	
+			
+			while((a % b) != 0) {
+				 
+				q = a % b;
+				a = b;
+				b = q;
+			}
+	
+			printf("%d", b);
+		}
+	
+		else if(b > a) {
+			
+			while((b % a) != 0) {
+				
+				q = b % a;
+				b = a;
+				a = q;
+			}
+			
+			printf("%d", a);
+		}
 	}
-	else if(b==0 && a!=0)
-	{
+	
+	else if(a == 0 && b != 0) {
+		
+		printf("%d", b);
+	}
+	
+	else if(b == 0 && a != 0) {
 		printf("%d", a);
         }
-        else  if(a==0 && b==0)
-	{
+        
+	else  if(a == 0 && b == 0) {
 		printf("Invalid input");
-		
 	}
+	
 	return 0;
 }
-
-
-
