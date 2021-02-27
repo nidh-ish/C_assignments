2a057611c2f4a9019dde3cbeb2d0ae46c1fca549
Nidhish Bhimrajka <54209827+nidh-ish@users.noreply.github.com>
2021-02-27 17:33:14 +0530

    Update PrimeNumbers.c

--- PrimeNumbers.c
+++ PrimeNumbers.c
@@ -1,38 +1,44 @@
 #include<stdio.h>
-int main()
-{
-    int a,i,j,count;
-    count=0;
-    scanf("%d",&a);
+
+int main() {
+    
+    int a, i, j, count;
+    count = 0;
+    
+    scanf("%d", &a);
+    
     int ar[20000];
-    for(i=0;i<=a;i++)
-    {
+    
+    for(i = 0; i <= a; i++) {
+        
         ar[i]=1;
     }
     
-    if(a<=1)
-    {
+    if(a <= 1) {
+        
         printf("0");
         return 0;
-        
     }
+    
     {
-        for(i=2;i*i<=a;i++)
-        {
-       	   	for(j=2*i;j<=a;j+=i)
-            {
-                ar[j]=0;
+        for(i = 2; i * i <= a; i++) {
+            
+       	   	for(j = 2 * i; j <= a; j += i) {
+                
+                ar[j] = 0;
             }
         }
     }
-    for(int k=2;k<=a;k++)
-    {
-        if(ar[k]!=0)
-        {
+    
+    for(int k = 2; k <= a; k++) {
+        
+        if(ar[k] != 0) {
+            
             count++;
         }
-    
     }
-    printf("%d",count);
+    
+    printf("%d", count);
+    
     return 0;
 }
