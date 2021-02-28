c1ea99221c81a9cc44c15ee5763f2b3324d9142c
Nidhish Bhimrajka <54209827+nidh-ish@users.noreply.github.com>
2021-02-28 16:09:58 +0530

    Update ArrayReversal.c

--- ArrayReversal.c
+++ ArrayReversal.c
@@ -1,16 +1,21 @@
 #include<stdio.h>
-int main()
+int main() 
 {
 	int ar[10];  //here we define an array of 10 integer values 
+	
 	int i;  
-	for(i = 0; i < 10; i++)
-	{
+	
+	for(i = 0; i < 10; i++) {
+		
 		scanf("%d,", &ar[i]);  //here we scan the 10 integers
 	}
+	
 	printf("%d", ar[9]);  //here we print the last element first of all as per required by the question
-	for(i = 8; i >= 0 ; i--)
-	{
+	
+	for(i = 8; i >= 0 ; i--) {
+		
 		printf(" %d", ar[i]); //the rest of the elements are printed in the same reverse order
 	}
+	
 	return 0;
 }
