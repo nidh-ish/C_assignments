9810a5fc22ae82c72b9c9d424e9cd8ff1ed2f5a0
Nidhish Bhimrajka <54209827+nidh-ish@users.noreply.github.com>
2021-02-28 16:11:09 +0530

    Update LinearSearch.c

--- LinearSearch.c
+++ LinearSearch.c
@@ -1,24 +1,29 @@
 #include<stdio.h>
-int main()
+int main() 
 {
 	int ar[10];  //here we define array of ten integers
 	int i;
 	int x;
-	int flag=0;
-	for(i = 0; i<10; i++)
-	{
+	int flag = 0;
+	
+	for(i = 0; i < 10; i++) {
+		
 		scanf("%d,", &ar[i]); //here we scan all the ten integers and store them in the array
 	}
+	
 	scanf(" %d", &x);  //here we scan the number we need to search for in the array
-        for(i=0; i<10; i++)
-	{
-		if(x==ar[i]) //here we compare the element with the rest of them one by one and check whether its is same or not
+        
+	for(i = 0; i < 10; i++) {
+		
+		if(x == ar[i]) //here we compare the element with the rest of them one by one and check whether its is same or not
 		{
-			flag=1;
+			flag = 1;
 			i++;     
 			break;
 		}
         }
-	printf("%d %d",flag,i);  //here we print the number 1 if the elemet given is in the array and the number of operations we performed 
+	
+	printf("%d %d", flag, i);  //here we print the number 1 if the elemet given is in the array and the number of operations we performed 
+	
 	return 0;
 }
