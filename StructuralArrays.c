212106c2b14e55003a5c5128fe62be7c4b46030a
Nidhish Bhimrajka <54209827+nidh-ish@users.noreply.github.com>
2021-03-03 21:42:57 +0530

    Update StructuralArray.c

--- StructuralArrays.c
+++ StructuralArrays.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 struct details  //taking input of name, roll no, marks and age
 {
 	char name[100];
@@ -6,18 +7,24 @@ struct details  //taking input of name, roll no, marks and age
 	int age;
 	int marks;
 };
+
 int main()
 {
 	int n;  //taking input the number of students
+	
 	scanf("%d", &n);
+	
 	struct details s[n];
+
 	for(int i = 0; i < n; i++) //taking input of their data
 	{
 		scanf("\n%s %s %d %d", s[i].name, s[i].roll, &s[i].age, &s[i].marks);
 	}
+	
 	for(int j = 0; j < n; j++) //printing the data of the students
 	{
 		printf("%s\n%s\n%d\n%d\n", s[j].name, s[j].roll, s[j].age, s[j].marks);
 	}
+	
 	return 0;
 }
