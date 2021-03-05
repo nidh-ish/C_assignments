c105db29a3309ea734bea9358e4ff7ced8e73805
Nidhish Bhimrajka <54209827+nidh-ish@users.noreply.github.com>
2021-03-05 11:20:38 +0530

    Update CommandLineSum.c

--- CommandLineSum.c
+++ CommandLineSum.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(int argc, char *argv[]) //using command line arguments
+
+int main(int argc, char *argv[]) 		//using command line arguments
 {
 	int sum = 0;  
-	printf("%d", argc - 1); // printing the number of arguments
+	
+	printf("%d", argc - 1); 		// printing the number of arguments
+	
 	for(int i = 1; i < argc; i++)
 	{
-		sum = sum + atoi(argv[i]); // finding the sum of arguments
+		sum = sum + atoi(argv[i]); 	// finding the sum of arguments
 	}
-	printf(" %d", sum); // printing the value of sum
+	
+	printf(" %d", sum); 			// printing the value of sum
+	
 	return 0;
 }
-
